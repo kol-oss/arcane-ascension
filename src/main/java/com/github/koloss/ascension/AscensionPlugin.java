95145ef7af0f3351a33757167f1de5ac3b3ac6c7
kol-oss <kolyakhozhajinov@gmail.com>
2025-11-28 14:16:36 +0200

    Add and connect item listener

--- src/main/java/com/github/koloss/ascension/AscensionPlugin.java
+++ src/main/java/com/github/koloss/ascension/AscensionPlugin.java
@@ -1,11 +1,28 @@
 package com.github.koloss.ascension;
 
+import com.github.koloss.ascension.listener.ItemListener;
+import com.github.koloss.ascension.utils.InventoryService;
+import com.github.koloss.ascension.service.ItemService;
+import com.github.koloss.ascension.utils.impl.InventoryServiceImpl;
+import com.github.koloss.ascension.service.impl.ItemServiceImpl;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AscensionPlugin extends JavaPlugin {
+    private Listener createItemListener() {
+        InventoryService inventoryService = new InventoryServiceImpl();
+        ItemService itemService = new ItemServiceImpl(inventoryService);
+
+        return new ItemListener(itemService);
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        PluginManager manager = Bukkit.getPluginManager();
+
+        manager.registerEvents(createItemListener(), this);
     }
 
     @Override
