69dd4ad1109d3f9fa9a0f393d7917f8709b00a77
kol-oss <kolyakhozhajinov@gmail.com>
2025-11-28 14:15:22 +0200

    Add common ascension data

---
+++ src/main/java/com/github/koloss/ascension/common/AscensionParams.java
@@ -0,0 +1,9 @@
+package com.github.koloss.ascension.common;
+
+import org.bukkit.NamespacedKey;
+
+public final class AscensionParams {
+    public static final String NAMESPACE = "ascension";
+
+    public static final NamespacedKey MATERIAL_KEY = new NamespacedKey(NAMESPACE, "material");
+}
