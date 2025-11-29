ed8902e253ececd0e441fef76e6e138bcc2346b9
kol-oss <kolyakhozhajinov@gmail.com>
2025-11-29 16:40:59 +0200

    Add find all and custom methods to repositories

--- src/main/java/com/github/koloss/ascension/repository/impl/ReputationRepositoryImpl.java
+++ src/main/java/com/github/koloss/ascension/repository/impl/ReputationRepositoryImpl.java
@@ -2,10 +2,17 @@ package com.github.koloss.ascension.repository.impl;
 
 import com.github.koloss.ascension.database.DatabaseManager;
 import com.github.koloss.ascension.mapper.ModelMapper;
+import com.github.koloss.ascension.model.DivineBranch;
 import com.github.koloss.ascension.model.Reputation;
 import com.github.koloss.ascension.repository.ReputationRepository;
 import com.github.koloss.ascension.repository.base.impl.BaseRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ReputationRepositoryImpl extends BaseRepository<Reputation, UUID> implements ReputationRepository {
@@ -27,6 +34,46 @@ public class ReputationRepositoryImpl extends BaseRepository<Reputation, UUID> i
 
     @Override
     protected String getUpdateString() {
-        return "UPDATE " + TABLE_NAME + " SET user_id = ?, branch = ?, reputation = ? WHERE id = ?";
+        return "UPDATE " + TABLE_NAME + " SET reputation = ? WHERE id = ?";
+    }
+
+    @Override
+    public Optional<Reputation> findByUserIdAndBranch(UUID userId, DivineBranch branch) {
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE user_id = ? AND branch = ?";
+
+        try (PreparedStatement ps = manager.getConnection().prepareStatement(query)) {
+            ps.setString(1, userId.toString());
+            ps.setString(2, branch.toString());
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (!rs.next()) {
+                    return Optional.empty();
+                }
+
+                return Optional.of(mapper.toEntity(rs));
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public List<Reputation> findAllByUserId(UUID userId) {
+        List<Reputation> result = new ArrayList<>();
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE user_id = ?";
+        try (PreparedStatement ps = manager.getConnection().prepareStatement(query)) {
+            ps.setString(1, userId.toString());
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    result.add(mapper.toEntity(rs));
+                }
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return result;
     }
 }
