45a190ceade6d4219e2a13a0a51b3117a6c1a6ab
kol-oss <kolyakhozhajinov@gmail.com>
2025-11-29 16:39:35 +0200

    Separate insert and update in mappers

--- src/main/java/com/github/koloss/ascension/mapper/impl/ReputationMapperImpl.java
+++ src/main/java/com/github/koloss/ascension/mapper/impl/ReputationMapperImpl.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ReputationMapperImpl implements ModelMapper<Reputation> {
     @Override
@@ -16,15 +17,21 @@ public class ReputationMapperImpl implements ModelMapper<Reputation> {
                 .id(UUID.fromString(rs.getString(1)))
                 .userId(UUID.fromString(rs.getString(2)))
                 .branch(DivineBranch.valueOf(rs.getString(3)))
-                .reputation(rs.getLong(4))
+                .reputation(new AtomicLong(rs.getLong(4)))
                 .build();
     }
 
     @Override
-    public void setPreparedValues(PreparedStatement ps, Reputation entity) throws SQLException {
+    public void setInsertValues(PreparedStatement ps, Reputation entity) throws SQLException {
         ps.setString(1, entity.getId().toString());
         ps.setString(2, entity.getUserId().toString());
         ps.setString(3, entity.getBranch().name());
-        ps.setLong(4, entity.getReputation());
+        ps.setLong(4, entity.getReputation().get());
+    }
+
+    @Override
+    public void setUpdateValues(PreparedStatement ps, Reputation entity) throws SQLException {
+        ps.setLong(1, entity.getReputation().get());
+        ps.setString(2, entity.getId().toString());
     }
 }
