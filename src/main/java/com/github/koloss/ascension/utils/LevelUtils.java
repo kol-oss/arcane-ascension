07691c1b68bc5d19a473ecff7c32f9ed457c9180
kol-oss <kolyakhozhajinov@gmail.com>
2025-12-02 04:31:03 +0200

    Remove and add number utils

---
+++ src/main/java/com/github/koloss/ascension/utils/LevelUtils.java
@@ -0,0 +1,19 @@
+package com.github.koloss.ascension.utils;
+
+import com.github.koloss.ascension.common.LevelParams;
+
+public class LevelUtils {
+    public static long getProgressOfNextLevel(long experience) {
+        return (long) getLevelFromProgress(experience) * LevelParams.XP_PR_LEVEL + LevelParams.XP_PR_LEVEL;
+    }
+
+    public static int getLevelFromProgress(long experience) {
+        return (int) (experience / LevelParams.XP_PR_LEVEL);
+    }
+
+    public static boolean isLevelOpen(int level, int userLevel, long experience) {
+        int expectedLevel = LevelUtils.getLevelFromProgress(experience);
+
+        return (userLevel + 1) == level && expectedLevel > userLevel;
+    }
+}
---
+++ src/main/java/com/github/koloss/ascension/utils/NumberUtils.java
@@ -0,0 +1,27 @@
+package com.github.koloss.ascension.utils;
+
+import java.util.TreeMap;
+
+public final class NumberUtils {
+    private final static TreeMap<Integer, String> ROMAN_MAP = new TreeMap<>();
+
+    static {
+        ROMAN_MAP.put(10, "X");
+        ROMAN_MAP.put(9, "IX");
+        ROMAN_MAP.put(5, "V");
+        ROMAN_MAP.put(4, "IV");
+        ROMAN_MAP.put(1, "I");
+    }
+
+    public static String toRoman(int number) {
+        if (number <= 0)
+            return "";
+
+        int largestValue = ROMAN_MAP.floorKey(number);
+        if (number == largestValue) {
+            return ROMAN_MAP.get(number);
+        }
+
+        return ROMAN_MAP.get(largestValue) + toRoman(number - largestValue);
+    }
+}
