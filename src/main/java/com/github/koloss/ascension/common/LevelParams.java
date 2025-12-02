c7ee1ff88b23c1f827c77383182dfed0485317bc
kol-oss <kolyakhozhajinov@gmail.com>
2025-12-03 00:38:15 +0200

    Add follow button and level description

--- src/main/java/com/github/koloss/ascension/common/LevelParams.java
+++ src/main/java/com/github/koloss/ascension/common/LevelParams.java
@@ -1,7 +1,9 @@
 package com.github.koloss.ascension.common;
 
 public final class LevelParams {
-    public static final int XP_PR_LEVEL = 50;
     public static final int MIN_LEVEL = 1;
     public static final int MAX_LEVEL = 23;
+
+    public static final int XP_PR_LEVEL = 50;
+    public static final int BUFF_PER_LEVEL = 2;
 }
