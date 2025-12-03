d2fe3c3218da354fd6e23fed322e61c95b3a360d
kol-oss <kolyakhozhajinov@gmail.com>
2025-12-04 01:11:26 +0200

    Add modifiers for all skill types

--- src/main/java/com/github/koloss/ascension/constant/LevelConstants.java
+++ src/main/java/com/github/koloss/ascension/constant/LevelConstants.java
@@ -4,6 +4,5 @@ public final class LevelConstants {
     public static final int MIN_LEVEL = 1;
     public static final int MAX_LEVEL = 23;
 
-    public static final int XP_PR_LEVEL = 50;
-    public static final int BUFF_PER_LEVEL = 2;
+    public static final int PROGRESS_PER_LEVEL = 50;
 }
--- src/main/java/com/github/koloss/ascension/utils/LevelUtils.java
+++ src/main/java/com/github/koloss/ascension/utils/LevelUtils.java
@@ -4,20 +4,16 @@ import com.github.koloss.ascension.constant.LevelConstants;
 
 public final class LevelUtils {
     public static long getProgressOfNextLevel(long progress) {
-        return (long) getLevelFromProgress(progress) * LevelConstants.XP_PR_LEVEL + LevelConstants.XP_PR_LEVEL;
+        return (long) getLevelFromProgress(progress) * LevelConstants.PROGRESS_PER_LEVEL + LevelConstants.PROGRESS_PER_LEVEL;
     }
 
     public static int getLevelFromProgress(long progress) {
-        return (int) (progress / LevelConstants.XP_PR_LEVEL);
+        return (int) (progress / LevelConstants.PROGRESS_PER_LEVEL);
     }
 
     public static boolean isLevelOpen(int level, int currLevel, long progress) {
         int expectedLevel = LevelUtils.getLevelFromProgress(progress);
 
         return (currLevel + 1) == level && expectedLevel > currLevel;
-    }
-
-    public static int getBuffForLevel(int level) {
-        return level * LevelConstants.BUFF_PER_LEVEL;
     }
 }
